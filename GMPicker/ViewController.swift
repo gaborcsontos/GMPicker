c24fa6aff625dcce18b4d6a242da70cb6fa8f4d9
Gabor <acsontosgabor@gmail.com>
2016-08-03 11:49:45 +0100

    Initial Commit

---
+++ GMPicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GMPicker
+//
+//  Created by Gabor Csontos on 8/3/16.
+//  Copyright © 2016 GabeMajorszki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
